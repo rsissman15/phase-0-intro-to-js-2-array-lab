0deaf902f23e582b8d3d8e8909934e34086971f9
Rebecca Sissman <rsissman15@aol.com>
2021-12-07 12:53:11 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(-1);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(0);
+    return cats;
+}
+
+function appendCat(name){
+    const catsappend = [...cats,name];
+    return catsappend;
+}
+
+function prependCat(name){
+    const catsprepend = [name,...cats];
+    return catsprepend;
+}
+
+function removeLastCat(name){
+    const removeLastcat=cats.slice(0,cats.length-1);
+    return removeLastcat;
+}
+
+function removeLastCat(name){
+    const removeLastcat=cats.slice(0,cats.length-1);
+    return removeLastcat;
+}
+
+function removeLastCat(name){
+    const removeLastcat=cats.slice(0,cats.length-1);
+    return removeLastcat;
+}
+
+function removeFirstCat(name){
+    const removeFirstcat=cats.slice(1);
+    return removeFirstcat;
+}
+
+console.log(removeFirstCat("Tina"));
+
